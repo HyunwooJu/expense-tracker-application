5e636bbf18901df0b1b0612194e3dca336fbe990
HyunwooJu <soc05104@gmail.com>
2024-05-27 17:17:15 +0900

    props-drilling 기능 구현 완료

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,30 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
-ReactDOM.render(
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: #f8f9fa;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: "#007bff",
+    secondary: "#6c757d",
+  },
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
